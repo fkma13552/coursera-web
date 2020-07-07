7ed667dd53f17c78cdf4b70d8a1058c03ba1ee70
fkma13552 <ozonplay123@gmail.com>
2020-07-07 11:57:44 +0300

    My first JS code

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,9 @@
+(function() {
+var helloSpeaker = {
+  speak: function(name) {
+    console.log(speakWord + " " + name);
+    }
+  }
+var speakWord = "Hello";
+window.helloSpeaker = helloSpeaker;
+})();
